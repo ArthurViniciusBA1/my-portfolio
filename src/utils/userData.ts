e7ace636687c7bb13d44dfad067906fd9541ba12
unknown <arthurnaocopia@gmail.com>
2023-05-19 12:24:38 -0300

    feat/Initial project

---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Arthur Vinícius",
+  githubUser: "ArthurViniciusBA1",
+  linkedinUser: "ferreiraarthurvinicius",
+  whatsappNumber: "3197079666",
+  emailUser: "arthurnaocopia@gmail.com",
+  instagramUser: "samuelleaoui",
+  facebookUser: "",
+};
